8f3e9e99375be69c43750f0f796068ff63586259
kmieszala <mkonrad14@gmail.com>
2013-05-27 12:41:59 +0200

    dodanie jquery

--- js/waz.js
+++ js/waz.js
@@ -1,4 +1,40 @@
-function Waz()
+exports.Waz = Waz =function()
 {
-    this.WAZ_DL = 3;
-}
+	this.WAZ_LENGTH = 8;
+};
+Waz.prototype.init = function (playerId)
+{
+    this.currentlength = this.WAZ_LENGTH;
+    this.playerId = playerId;
+    this.kills = 0;
+    this.elements = [];
+};
+Waz.prototype.moveBlock = function(i)
+{
+    this.elements[i].x = this.elements[i + 1].x;
+    this.elements[i].y = this.elements[i + 1].y;
+};
+Waz.prototype.moveHead = function(i, direction)
+{
+    var length = this.elements.length;
+    var head = this.elements[length];
+    switch (this.direction)
+    {
+        case 'left':
+                head.x--;
+        break;
+
+        case 'right':
+                head.x++;
+        break;
+
+        case 'down':
+                head.y++;
+        break;
+
+        case 'up':
+                head.y--;
+        break;
+
+    }
+};
